7bfcc4344bbc1f8fe92c03766399b855a4db5e17
Lê Hoàng Anh <anhhl@acaziasoft.com>
2020-04-04 20:05:30 +0700

    Initial Commit

---
+++ InsCard/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InsCard
+//
+//  Created by Lê Hoàng Anh on 4/4/20.
+//  Copyright © 2020 Lê Hoàng Anh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
